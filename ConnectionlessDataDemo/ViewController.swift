bb9b2a825fbea616d505f677aa1bed057dfa1dae
John <john.groenhof@gmail.com>
2018-03-21 13:39:46 -0600

    Initial Commit

---
+++ ConnectionlessDataDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConnectionlessDataDemo
+//
+//  Created by John on 3/21/18.
+//  Copyright © 2018 KS Technologies, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
